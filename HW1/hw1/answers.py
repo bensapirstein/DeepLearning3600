ec14ce39fc70ebe5c5b9162beffe651392bc11c9
Ben Sapirstein <ben.sapirstein@gmail.com>
2023-05-16 00:12:29 +0300

    answered q4 questions
    Add explanation and observations from residual plots
    Clarify impact of non-linear features in linear regression model
    Include code example for using non-linear features in regression

--- HW1/hw1/answers.py
+++ HW1/hw1/answers.py
@@ -104,22 +104,107 @@ An equation: $e^{i\pi} -1 = 0$
 # Part 4 answers
 
 part4_q1 = r"""
-**Your answer:**
+An ideal residual plot should show randomness, constant variance (homoscedasticity), a mean of zero, and independence. 
+Randomness indicates that the model captures the relationship between variables without violating assumptions. 
+Constant variance suggests that the spread of residuals is consistent across predicted values. 
+A mean of zero indicates unbiased predictions. 
+Independence means that residuals at one point do not provide information about residuals at other points. 
 
-The ideal pattern in a residual plot is a plot where 
+Based on the provided residual plots, we can make several observations regarding the fitness of the trained model.
+Firstly, the plots indicate that the model performs relatively well in predicting mid and low-priced houses, as the residuals exhibit a good fit in those ranges.
+However, there seems to be less accuracy when predicting high-priced houses. 
+This discrepancy may be attributed to a limited representation of high prices in the available data, which affects the randomness of the residuals.
+Despite this limitation, a positive aspect is that the residuals are centered around a zero mean, suggesting that the model does not suffer from a significant problem of over- or underestimation.
+
+```
+fig, axes = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(20,10))
+
+y_pred = hw1linreg.fit_predict_dataframe(model,  df_boston, 'MEDV', top_feature_names)
+
+
+plot_residuals(y, y_pred, ax=axes[0])
+
+# calculate residual mean
+res = y - y_pred
+res_mean = res.mean()
+
+# plot mean line in green
+axes[0].hlines(y=res_mean, xmin=y_pred.min(), xmax=y_pred.max(), color='green', lw=3)
+
+# Use the best hyperparameters
+model.set_params(**best_hypers)
+
+# Train best model on full training set
+y_pred_train, mse, rsq = linreg_boston(model, x_train, y_train)
+print(f'train: mse={mse:.2f}, rsq={rsq:.2f}')
+ax = plot_residuals(y_train, y_pred_train, ax=axes[1], res_label='train')
+
+# Evaluate on test set
+y_pred_test, mse, rsq = linreg_boston(model, x_test, y_test, fit=False)
+print(f'test:  mse={mse:.2f}, rsq={rsq:.2f}')
+ax = plot_residuals(y_test, y_pred_test, ax=ax, res_label='test')
+
+# plot mean line in green
+axes[1].hlines(y=res_mean, xmin=y_pred.min(), xmax=y_pred.max(), color='green', lw=3)
+
+```
 
 
 """
 
 part4_q2 = r"""
-**Your answer:**
+Adding non-linear features to the data would result in a model that is no longer a pure linear regression model.
+Linear regression assumes a linear relationship between the predictors and the response variable.
+By introducing non-linear features, the relationship between the predictors and the response becomes non-linear, making it a form of polynomial regression or non-linear regression.
+So, adding non-linear features changes the nature of the regression model.
 
+Yes, by adding non-linear features, we can capture and fit non-linear functions of the original features.
+For example, if we have a feature 'x', we can create additional features like 'x^2', 'x^3', or even more complex non-linear transformations like 'sqrt(x)' or 'log(x)'.
+These non-linear features allow the model to capture and represent non-linear relationships between the predictors and the response.
+
+```
+from sklearn.linear_model import LinearRegression
+
+# Generate a toy dataset with a cubic relationship
+x = np.linspace(-10, 10, 100)
+y = x**3 + np.random.normal(scale=50, size=100)
+
+# Fit a linear regression model with x^3 as a feature
+model = LinearRegression()
+X = pd.DataFrame({'x': x, 'x_cubed': x**3})
+model.fit(X, y)
+
+# Print the coefficients for x and x^3
+print(model.coef_)
+
+# Make predictions on new data
+x_new = np.linspace(-15, 15, 100)
+X_new = pd.DataFrame({'x': x_new, 'x_cubed': x_new**3})
+y_pred = model.predict(X_new)
+
+
+plt.scatter(x, y)
+plt.plot(x_new, y_pred, color='red')
+plt.show()
+```
+
+In the case of a linear classification model, adding non-linear features would lead to a decision boundary that is no longer a simple hyperplane.
+The decision boundary would become a more complex shape, potentially curved or nonlinear, due to the presence of the non-linear features.
+This is because the non-linear features introduce additional dimensions and complexity to the model, allowing it to separate classes that may not be linearly separable in the original feature space.
+We can think of polynomial features as an additional layer to our model which no longer keeps it a linear model.
 
 """
 
 part4_q3 = r"""
-**Your answer:**
+1. This is beneficial for cross-validation as it allows us to explore a wider range of hyperparameter values with a smaller number of samples.
+This is particularly useful for regularization parameters like $\lambda$, where the optimal value may lie on a logarithmic scale. 
 
+2. The outer loop iterates over the combinations of degree and $\lambda$, resulting in $d \times l$ iterations.
+Within each iteration of the outer loop, the model is fitted $k_{\text{folds}}$ times, where $k_{\text{folds}}$ is the number of folds used for cross-validation.
+Therefore, the total number of times the model is fitted is $d \times l \times k_{\text{folds}}$.
+There are $d$ degrees and $l$ $\lambda$ values, and $k_{\text{folds}}$ is set to 3, the model will be fitted $d \times l \times 3$ times in total.
+degree_range has 3 values and the lambda_range has 20 values, the model will be fitted $3 \times 20 \times 3 = 180$ times in total.
+Following cross-validation, the model undergoes a final fine-tuned fit on the complete training set, utilizing the selected hyperparameters.
 
 """
 
